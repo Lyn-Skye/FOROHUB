7e88132ed54cba0a02f297a34c615725e712d8a2
Lyn-Skye <qfbhildalacaze@gmail.com>
2024-07-12 00:04:06 -0500

    1- Creada la clase Respuesta.java

---
+++ src/main/java/com/alura/reto/ForoHub/foro/Respuesta.java
@@ -0,0 +1,10 @@
+package com.alura.reto.ForoHub.foro;
+
+public class Respuesta {
+    private Long id;
+    private String mensaje;
+    private String topico;
+    private String fechaDeCreacion;
+    private String autor;
+    private String solucion;
+}
