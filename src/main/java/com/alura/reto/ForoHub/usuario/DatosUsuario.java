4c4d4e6e4c011f41e99de8e66924161f3fc9646f
Lyn-Skye <qfbhildalacaze@gmail.com>
2024-07-12 00:02:55 -0500

    1- Creado el DTO

---
+++ src/main/java/com/alura/reto/ForoHub/usuario/DatosUsuario.java
@@ -0,0 +1,10 @@
+package com.alura.reto.ForoHub.usuario;
+
+public record DatosUsuario(
+        Long id,
+        String nombre,
+        String email,
+        String password
+//        Perfil perfil
+) {
+}
