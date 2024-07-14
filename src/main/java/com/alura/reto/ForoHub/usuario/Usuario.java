6cdc9bd0359fb0e205aec8d2f169e5f88dd7a3bd
Lyn-Skye <qfbhildalacaze@gmail.com>
2024-07-14 03:03:21 -0500

    1- Creada la función de actualizar datos usuario 3- Creados los constructores

--- src/main/java/com/alura/reto/ForoHub/usuario/Usuario.java
+++ src/main/java/com/alura/reto/ForoHub/usuario/Usuario.java
@@ -1,7 +1,6 @@
 package com.alura.reto.ForoHub.usuario;
 
 import jakarta.persistence.*;
-import com.alura.reto.ForoHub.foro.Topico;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -18,9 +17,30 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
-    private String email;
-    private String password;
-    @OneToMany//(mappedBy = "topico", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Topico topico;
+    private String email; //posiblemente habrá que agregarle @NotBlank y @Email
+    private String clave;
 //    private Perfil perfil;
+
+    public Usuario(DatosPublicosUsuario datosPublicosUsuario) {
+        this.id = datosPublicosUsuario.id();
+        this.nombre = datosPublicosUsuario.nombre();
+    }
+
+    public Usuario(DatosRegistroUsuario datosRegistroUsuario){
+        this.nombre = datosRegistroUsuario.nombre();
+        this.email = datosRegistroUsuario.email();
+        this.clave = datosRegistroUsuario.clave();
+    }
+
+    public void actualizarDatosUsuario(DatosActualizarUsuario datosActualizarUsuario) {
+        if(datosActualizarUsuario.nombre()!=null){
+            this.nombre = datosActualizarUsuario.nombre();
+        }
+        if(datosActualizarUsuario.email()!=null){
+            this.email = datosActualizarUsuario.email();
+        }
+        if(datosActualizarUsuario.clave()!=null){
+            this.clave = datosActualizarUsuario.clave();
+        }
+    }
 }
