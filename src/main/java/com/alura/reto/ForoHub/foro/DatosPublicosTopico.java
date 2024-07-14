c60b800853eb25b22db4cb4306447086aad1ac0b
Lyn-Skye <qfbhildalacaze@gmail.com>
2024-07-14 02:58:26 -0500

    1- Creado el record DatosPublicosTopico.java con los datos que recibirá

--- src/main/java/com/alura/reto/ForoHub/foro/DatosPublicosTopico.java
+++ src/main/java/com/alura/reto/ForoHub/foro/DatosPublicosTopico.java
@@ -1,9 +1,18 @@
 package com.alura.reto.ForoHub.foro;
 
+import com.alura.reto.ForoHub.usuario.Usuario;
+import jakarta.validation.constraints.NotBlank;
+
 public record DatosPublicosTopico(
-        Long idUsuario,
-        String mensaje,
-        String nombreCurso,
-        String titulo
+        @NotBlank Long id,
+        @NotBlank String mensaje,
+        @NotBlank String nombreCurso,
+        @NotBlank String titulo
+        //falta incluir la fecha actual
 ) {
+
+    public DatosPublicosTopico(Topico topico){
+        this(topico.getId(), topico.getMensaje(), topico.getNombreCurso(), topico.getTitulo());
+    }
+
 }
