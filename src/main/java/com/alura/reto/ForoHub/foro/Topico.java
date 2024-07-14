9b777dce1b23bb4cb522d61cbf0e69edcb4dfdc0
Lyn-Skye <qfbhildalacaze@gmail.com>
2024-07-14 03:00:52 -0500

    1- Creadas la funcion de actualizarDatosTopico

--- src/main/java/com/alura/reto/ForoHub/foro/Topico.java
+++ src/main/java/com/alura/reto/ForoHub/foro/Topico.java
@@ -1,12 +1,16 @@
 package com.alura.reto.ForoHub.foro;
 
+import com.alura.reto.ForoHub.usuario.DatosPublicosUsuario;
 import com.alura.reto.ForoHub.usuario.Usuario;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Table(name = "topicos")
+@Entity(name = "Topico")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,9 +24,28 @@ public class Topico {
     private String fechaCreacion;
     @ManyToOne
     private Usuario usuario;
+    private String nombreCurso;
+    private Long idUsuario;
+    private String nombre;
 
-    public Topico(DatosPublicosTopico datosPublicosTopico) {
-        this.
+    public Topico(DatosPublicosTopico datosPublicosTopico, DatosPublicosUsuario datosPublicosUsuario) {
+        this.mensaje= datosPublicosTopico.mensaje();
+        this.nombreCurso = datosPublicosTopico.nombreCurso();
+        this.titulo = datosPublicosTopico.titulo();
+        this.idUsuario = datosPublicosUsuario.id();
+        this.nombre = datosPublicosUsuario.nombre();
+    }
+
+    public void actualizarDatosTopico(DatosActualizarTopico datosActualizarTopico) {
+        if(datosActualizarTopico.mensaje()!=null){
+            this.mensaje = datosActualizarTopico.mensaje();
+        }
+        if(datosActualizarTopico.nombreCurso()!=null){
+            this.nombreCurso = datosActualizarTopico.nombreCurso();
+        }
+        if(datosActualizarTopico.titulo()!=null){
+            this.titulo = datosActualizarTopico.titulo();
+        }
     }
 //    private String status;
 //    private Curso curso;
