1b04789251f301ba01e1535ae66f9ba0724d0a12
Lyn-Skye <qfbhildalacaze@gmail.com>
2024-07-14 22:45:13 -0500

    1- Eliminados campos innecesarios

--- src/main/resources/db/migration/V2__create-table-topicos.sql
+++ src/main/resources/db/migration/V2__create-table-topicos.sql
@@ -1,9 +1,11 @@
 create table topicos(
 
     id bigint not null auto_increment,
-    titulo varchar (100) not null unique,
+    titulo varchar (200) not null unique,
     mensaje varchar (500) not null unique,
-    clave varchar(100) not null,
+    fechaCreacion datetime not null,
+    nombreCurso varchar(100) not null,
 
 primary key (id)
+
 );
