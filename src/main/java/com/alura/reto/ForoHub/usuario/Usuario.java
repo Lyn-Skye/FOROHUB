453fd178a3b6bee9a834a97f62c5c0e571b195c5
Lyn-Skye <qfbhildalacaze@gmail.com>
2024-07-14 22:43:46 -0500

    1- Clase agregada la parte del verificacion de seguridad

--- src/main/java/com/alura/reto/ForoHub/usuario/Usuario.java
+++ src/main/java/com/alura/reto/ForoHub/usuario/Usuario.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
@@ -12,14 +18,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of="Id") //usa el parametro ID para las comparaciones entre usuarios
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String email; //posiblemente habrá que agregarle @NotBlank y @Email
     private String clave;
-//    private Perfil perfil;
 
     public Usuario(DatosPublicosUsuario datosPublicosUsuario) {
         this.id = datosPublicosUsuario.id();
@@ -42,5 +47,40 @@ public class Usuario {
         if(datosActualizarUsuario.clave()!=null){
             this.clave = datosActualizarUsuario.clave();
         }
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return clave;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
