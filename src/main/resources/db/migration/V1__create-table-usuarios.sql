ff297e9022ee9b89c8b3bcdfd46266d6f3909f72
Lyn-Skye <qfbhildalacaze@gmail.com>
2024-07-14 22:44:50 -0500

    1- Aumentados los caracteres para clave

--- src/main/resources/db/migration/V1__create-table-usuarios.sql
+++ src/main/resources/db/migration/V1__create-table-usuarios.sql
@@ -3,7 +3,9 @@ create table usuarios(
     id bigint not null auto_increment,
     nombre varchar (100) not null,
     email varchar (100) not null unique,
-    clave varchar(100) not null,
+    clave varchar(500) not null,
 
 primary key (id)
 );
+
+insert into usuarios(id, nombre, email, clave) values(1, "Bowser", "Bowser@BowserBoss.com", "$2a$10$mk9UsG/X2.4PI1AaYJPib.i/yXRfXvK9Kq22RPIOZX52a3GjkhWTa");
