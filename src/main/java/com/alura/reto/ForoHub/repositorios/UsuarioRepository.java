946a49ecc9ed0f10b6cbff91bb1a059dba4cf0f4
Lyn-Skye <qfbhildalacaze@gmail.com>
2024-07-14 22:44:08 -0500

    1- Agregada la anotacion de repositorio y la funcion de buscar por email

--- src/main/java/com/alura/reto/ForoHub/repositorios/UsuarioRepository.java
+++ src/main/java/com/alura/reto/ForoHub/repositorios/UsuarioRepository.java
@@ -2,6 +2,11 @@ package com.alura.reto.ForoHub.repositorios;
 
 import com.alura.reto.ForoHub.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByEmail(String username);
 }
