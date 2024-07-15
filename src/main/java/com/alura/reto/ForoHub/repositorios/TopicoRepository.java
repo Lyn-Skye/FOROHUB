357b7064ef61068649fe7a32ed5b80f6ed953834
Lyn-Skye <qfbhildalacaze@gmail.com>
2024-07-14 22:43:03 -0500

    1- Clase movida a la carpeta topico

--- src/main/java/com/alura/reto/ForoHub/repositorios/TopicoRepository.java
+++ src/main/java/com/alura/reto/ForoHub/repositorios/TopicoRepository.java
@@ -1,7 +1,10 @@
 package com.alura.reto.ForoHub.repositorios;
 
-import com.alura.reto.ForoHub.foro.Topico;
+import com.alura.reto.ForoHub.topico.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TopicoRepository extends JpaRepository<Topico,Long> {
+
 }
